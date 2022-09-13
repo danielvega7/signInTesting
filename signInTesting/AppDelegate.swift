75de5e6340a94f5d239681892b6f7ebd874e3be7
danielvega7 <daniel.vega23@d155.org>
2022-09-13 09:51:36 -0500

    working

--- signInTesting/AppDelegate.swift
+++ signInTesting/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -30,6 +33,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: - Core Data stack
--- signInTesting/ViewController.swift
+++ signInTesting/ViewController.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseAuth
+import FirebaseAuthCombineSwift
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
@@ -14,6 +19,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signInButtonAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance.signIn(
+           with: signInConfig,
+           presenting: self
+       ) { user, error in
+           guard error == nil else { return }
+           guard let user = user else { return }
 
+           // Your user is signed in!
+       }
+    }
+    
+    
+    
+    /*guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+    // Create Google Sign In configuration object.
+    let config = GIDConfiguration(clientID: clientID)
+
+    // Start the sign in flow!
+    GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+      if let error = error {
+        // ...
+        return
+      }
+
+      guard
+        let authentication = user?.authentication,
+        let idToken = authentication.idToken
+      else {
+        return
+      }
+
+      let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                     accessToken: authentication.accessToken)
+
+      // ...
+    }
+    */
 }
 
