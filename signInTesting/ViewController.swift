5d52082e463906e3e87fe06f7282cda6daa5d172
danielvega7 <daniel.vega23@d155.org>
2022-09-14 09:52:11 -0500

    more work

--- signInTesting/ViewController.swift
+++ signInTesting/ViewController.swift
@@ -9,18 +9,32 @@ import UIKit
 import Firebase
 import FirebaseCore
 import FirebaseAuth
-import FirebaseAuthCombineSwift
 import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
     }
 
     @IBAction func signInButtonAction(_ sender: UIButton) {
-        GIDSignIn.sharedInstance.signIn(
+       
+    }
+    
+    
+    
+    
+    
+    @IBAction func signInAction(_ sender: GIDSignInButton) {
+        
+        /*GIDSignIn.sharedInstance.signIn(
            with: signInConfig,
            presenting: self
        ) { user, error in
@@ -28,36 +42,100 @@ class ViewController: UIViewController {
            guard let user = user else { return }
 
            // Your user is signed in!
-       }
+       }*/
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+          if let error = error {
+              print(error.localizedDescription)
+            return
+          }
+
+          guard
+            let authentication = user?.authentication,
+            let idToken = authentication.idToken
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: authentication.accessToken)
+
+       /*     Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                  let authError = error as NSError
+                    if isMFAenabled, authError.code == AuthErrorCode.secondFactorRequired.rawValue {
+                    // The user is a multi-factor user. Second factor challenge is required.
+                    let resolver = authError
+                      .userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
+                    var displayNameString = ""
+                    for tmpFactorInfo in resolver.hints {
+                      displayNameString += tmpFactorInfo.displayName ?? ""
+                      displayNameString += " "
+                    }
+                    self.showTextInputPrompt(
+                      withMessage: "Select factor to sign in\n\(displayNameString)",
+                      completionBlock: { userPressedOK, displayName in
+                        var selectedHint: PhoneMultiFactorInfo?
+                        for tmpFactorInfo in resolver.hints {
+                          if displayName == tmpFactorInfo.displayName {
+                            selectedHint = tmpFactorInfo as? PhoneMultiFactorInfo
+                          }
+                        }
+                        PhoneAuthProvider.provider()
+                          .verifyPhoneNumber(with: selectedHint!, uiDelegate: nil,
+                                             multiFactorSession: resolver
+                                               .session) { verificationID, error in
+                            if error != nil {
+                              print(
+                                "Multi factor start sign in failed. Error: \(error.debugDescription)"
+                              )
+                            } else {
+                              self.showTextInputPrompt(
+                                withMessage: "Verification code for \(selectedHint?.displayName ?? "")",
+                                completionBlock: { userPressedOK, verificationCode in
+                                  let credential: PhoneAuthCredential? = PhoneAuthProvider.provider()
+                                    .credential(withVerificationID: verificationID!,
+                                                verificationCode: verificationCode!)
+                                  let assertion: MultiFactorAssertion? = PhoneMultiFactorGenerator
+                                    .assertion(with: credential!)
+                                  resolver.resolveSignIn(with: assertion!) { authResult, error in
+                                    if error != nil {
+                                      print(
+                                        "Multi factor finanlize sign in failed. Error: \(error.debugDescription)"
+                                      )
+                                    } else {
+                                      self.navigationController?.popViewController(animated: true)
+                                    }
+                                  }
+                                }
+                              )
+                            }
+                          }
+                      }
+                    )
+                  } else {
+                    print(error.localizedDescription)
+                    return
+                  }
+                  // ...
+                  return
+                }
+                // User is signed in
+                // ...
+            }
+           */
+        }
+        
+       
+        
     }
     
-    
-    
-    /*guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-
-    // Create Google Sign In configuration object.
-    let config = GIDConfiguration(clientID: clientID)
-
-    // Start the sign in flow!
-    GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
-
-      if let error = error {
-        // ...
-        return
-      }
-
-      guard
-        let authentication = user?.authentication,
-        let idToken = authentication.idToken
-      else {
-        return
-      }
-
-      let credential = GoogleAuthProvider.credential(withIDToken: idToken,
-                                                     accessToken: authentication.accessToken)
-
-      // ...
-    }
-    */
 }
 
